6f9e029cfaf6600f05f38d4d303229f6eb1266d3
Andres Reyes <oreyes@cads.com.gt>
2018-05-31 07:56:25 -0600

    Ultimos cambios angular

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -4,6 +4,8 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
 import { ProgessComponent } from "./progess/progess.component";
 import { Graficas1Component } from "./graficas1/graficas1.component";
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from "./promesas/promesas.component";
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 const paegesRoutes: Routes = [
   {
@@ -13,6 +15,8 @@ const paegesRoutes: Routes = [
       { path: "dashboard", component: DashboardComponent },
       { path: "progress", component: ProgessComponent },
       { path: "graficas1", component: Graficas1Component },
+      { path: "promesas", component: PromesasComponent },
+      { path: "rxjs", component: RxjsComponent },
       { path: "account-settings", component: AccountSettingsComponent },
       { path: "", redirectTo: "/dashboard", pathMatch: "full" }
     ]
